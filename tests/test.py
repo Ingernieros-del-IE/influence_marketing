d65ac4327ef6b95714e7eb39ac00b15d7f02b36c
Bvallegc <116285501+Bvallegc@users.noreply.github.com>
2023-03-01 21:21:49 +0100

    Tests_passed

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 import pandas as pd 
 
-df = pd.read_csv('/Users/beltran/Desktop/recoData')
+df = pd.read_csv('Your_data_path')
+
+print(df.columns)
 
 def engagementRateSumTest(self): # Here we will test that the engagement rate operation works as it should. 
-    assert(df['Authentic Engagement']/df['Followers'])
+    assert(df['Authentic engagement']/df['Followers'])
 
 def followersLimitTest(self): # Here we are testing that no item(influencer) is smaller than 100000 followers. 
     assert len(df['Followers']) > 10000
