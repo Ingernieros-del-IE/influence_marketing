3549d20c428399ca8d040bdc2aa8024c1f66567f
bvallegc <bvalle.ieu2020@student.ie.edu>
2023-04-16 15:30:02 +0200

    changes to data types

--- test/test.py
+++ test/test.py
@@ -4,12 +4,12 @@ import pandas as pd
 @pytest.fixture
 def df():
     # Replace the link below with your own shareable link
-    file_link = "https://drive.google.com/file/d/1jOMJc8lQwmLYyyPaQO-UysZoauroCVDM/view?usp=share_link"
+    file_link = "https://drive.google.com/file/d/1Gb2B3QMujysVqQY1G1OjoS3BhFWCF5gu/view?usp=sharing"
     return pd.read_csv(file_link)
 
 def test_engagementRateSum(df):
     # Here we will test that the engagement rate operation works as it should.
-    assert df['user_likes_mean'].mean() > 500
+    assert df['user_likes_mean'] > 500
 
 def test_followersLimit(df):
     # Here we are testing that no item (influencer) has less than 10000 followers.
